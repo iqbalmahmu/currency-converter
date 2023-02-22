bf9c19f2a0874c2402ec5bdb808771e355035a8e
rad <mdrad43@gmail.com>
2023-02-22 17:52:36 +0800

    commit aded

---
+++ app.js
@@ -0,0 +1,28 @@
+const handleClick = document.getElementById("handleClick");
+
+function convert() {
+  // Get input values
+  var amount = document.getElementById("amount").value;
+  var from = document.getElementById("from").value;
+  var to = document.getElementById("to").value;
+
+  // Define conversion rates
+  var rates = {
+    USD: 1,
+    EUR: 0.82,
+    GBP: 0.72,
+  };
+
+  // Calculate conversion
+  var result = (amount / rates[from]) * rates[to];
+
+  // Display result
+  document.getElementById("result").innerHTML =
+    amount + " " + from + " = " + result.toFixed(2) + " " + to;
+}
+
+// add event listners functionality...
+
+// const handleClick = document.getElementById("handleClick");
+
+handleClick.addEventListener("click", convert);
